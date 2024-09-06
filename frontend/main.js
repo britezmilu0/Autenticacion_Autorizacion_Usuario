9ed7b866fa8a3d8874b5a8c19a0ed26146814c66
milagros britez <britezmilu0@gmail.com>
2024-09-06 14:28:34 -0300

    Se hicieron los cambos correspondiente, se modifico la caperta conf, se creo la funcion register y el html para el mismo

--- frontend/main.js
+++ frontend/main.js
@@ -4,12 +4,10 @@
         credentials: 'include' // Importante para enviar las cookies de sesión
     })
 
-    console.log({ response })
-
-
     if (response.ok) {
         const data = await response.json();
-        document.getElementById('user-name').innerText = data.user.username;
+        console.log(data);
+        document.getElementById('user-name').innerText = data.user[0].username;
     } else {
         // Redirigir al usuario a la página de inicio de sesión
         window.location.href = 'index.html';
